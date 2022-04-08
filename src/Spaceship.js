91699916c9ebeaa959df3dceea8e1c9ee3adb210
taiwenlee <leetaiwen101@gmail.com>
2022-04-08 15:48:04 -0700

    added spaceships

---
+++ src/Spaceship.js
@@ -0,0 +1,17 @@
+class Spaceship extends Phaser.GameObjects.Sprite {
+   constructor(scene, x, y, texture, frame, pointValue) {
+      super(scene, x, y, texture, frame);
+      scene.add.existing(this);    // add object to existing scene
+      this.points = pointValue;     // store points value
+      this.moveSpeed = 3;           // pixels per frame
+   }
+
+   update() {
+      // move spaceship left
+      this.x -= this.moveSpeed;
+      // wrap around from left edge to right edge
+      if(this.x <= 0 - this.width) {
+         this.x = game.config.width;
+      }
+   }
+}
